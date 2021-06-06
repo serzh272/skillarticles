8c3843a36998c96e601d05c4a3cb8a6f83cd4899
serzh272 <serzh272@mail.ru>
2021-06-06 22:41:38 +0900

    HorizontalRuleSpan finished

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
@@ -5,6 +5,7 @@ import android.graphics.Typeface
 import android.text.SpannableStringBuilder
 import android.text.SpannedString
 import android.text.style.CharacterStyle
+import android.text.style.StrikethroughSpan
 import android.text.style.StyleSpan
 import androidx.core.text.buildSpannedString
 import androidx.core.text.inSpans
@@ -14,6 +15,7 @@ import ru.skillbranch.skillarticles.extensions.dpToIntPx
 import ru.skillbranch.skillarticles.extensions.dpToPx
 import ru.skillbranch.skillarticles.markdown.spans.BlockquotesSpan
 import ru.skillbranch.skillarticles.markdown.spans.HeaderSpan
+import ru.skillbranch.skillarticles.markdown.spans.HorizontalRuleSpan
 import ru.skillbranch.skillarticles.markdown.spans.UnorderedListSpan
 
 class MarkdownBuilder(context: Context) {
@@ -25,6 +27,7 @@ class MarkdownBuilder(context: Context) {
     private val colorDivider = context.getColor(R.color.color_divider)
     private val headerMarginTop = context.dpToPx(12)
     private val headerMarginBottom = context.dpToPx(8)
+    private val ruleWidth = context.dpToPx(2)
     fun markdownToSpan(string: String) : SpannedString{
         val markdown = MarkdownParser.parse(string)
         return buildSpannedString {
@@ -69,6 +72,18 @@ class MarkdownBuilder(context: Context) {
                         for (child in element.elements){
                             buildElement(child, builder)
                         }
+                    }
+                }
+                is Element.Strike ->{
+                    inSpans(StrikethroughSpan()){
+                        for (child in element.elements){
+                            buildElement(child, builder)
+                        }
+                    }
+                }
+                is Element.Rule ->{
+                    inSpans(HorizontalRuleSpan(ruleWidth, colorDivider)){
+                        append(element.text)
                     }
                 }
                 else -> append(element.text)
