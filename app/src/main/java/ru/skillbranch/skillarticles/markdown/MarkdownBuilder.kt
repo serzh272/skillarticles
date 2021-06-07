5e9b0e35994b1e631673a6c226cbca5b2b826690
serzh272 <serzh272@mail.ru>
2021-06-07 19:34:17 +0900

    IconLinkSpan finished

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
@@ -4,30 +4,32 @@ import android.content.Context
 import android.graphics.Typeface
 import android.text.SpannableStringBuilder
 import android.text.SpannedString
-import android.text.style.CharacterStyle
 import android.text.style.StrikethroughSpan
 import android.text.style.StyleSpan
+import android.text.style.URLSpan
+import androidx.core.content.ContextCompat
 import androidx.core.text.buildSpannedString
 import androidx.core.text.inSpans
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.attrValue
-import ru.skillbranch.skillarticles.extensions.dpToIntPx
 import ru.skillbranch.skillarticles.extensions.dpToPx
 import ru.skillbranch.skillarticles.markdown.spans.*
 
 class MarkdownBuilder(context: Context) {
-    private val cornerRadius = context.dpToPx(8)
+    private val strikeWidth: Float = context.dpToPx(4)
     private val colorOnSurface = context.attrValue(R.attr.colorOnSurface)
-    private val colorSurface = context.attrValue(R.attr.colorSurface)
-    private val gap = context.dpToPx(8)
-    private val bulletRadius = context.dpToPx(4)
-    private val quoteWidth = context.dpToPx(4)
+    private val opacityColorSurface = context.getColor(R.color.opacity_color_surface)
     private val colorSecondary = context.attrValue(R.attr.colorSecondary)
     private val colorPrimary = context.attrValue(R.attr.colorPrimary)
     private val colorDivider = context.getColor(R.color.color_divider)
+    private val cornerRadius = context.dpToPx(8)
+    private val gap = context.dpToPx(8)
+    private val bulletRadius = context.dpToPx(4)
+    private val quoteWidth = context.dpToPx(4)
     private val headerMarginTop = context.dpToPx(12)
     private val headerMarginBottom = context.dpToPx(8)
     private val ruleWidth = context.dpToPx(2)
+    private val linkIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_link_24)!!
     fun markdownToSpan(string: String) : SpannedString{
         val markdown = MarkdownParser.parse(string)
         return buildSpannedString {
@@ -87,7 +89,13 @@ class MarkdownBuilder(context: Context) {
                     }
                 }
                 is Element.InlineCode ->{
-                    inSpans(InlineCodeSpan(colorOnSurface, colorSurface,cornerRadius, gap)){
+                    inSpans(InlineCodeSpan(colorOnSurface, opacityColorSurface,cornerRadius, gap)){
+                        append(element.text)
+                    }
+                }
+                is Element.Link ->{
+                    inSpans(IconLinkSpan(linkIcon,gap,colorPrimary, strikeWidth),
+                    URLSpan(element.link)){
                         append(element.text)
                     }
                 }
